3cf82750959ae753f14d814e5a977983970a9c5a
Kevin-Illu <78170908+Kevin-Illu@users.noreply.github.com>
2022-11-15 22:06:06 -0600

    se agrego tailwind cli y se modifico el readme

--- src/app.js
+++ src/app.js
@@ -1,16 +1,7 @@
-// para realizar alguna tarea tienes que crear
-// un archivo para tu tarea en la carpeta que creas
-// conveniente 
-// ejemplo:
-// una llamada a una api iria en la carpeta helpers
-// componentes html en components
-
-// importanciones de archivos aqui.
 import buscarPokemon from './helpers/consumoAPI.js';
 
-
-// todas la funciones se llaman aqui.
-document.addEventListener('DOMContentLoaded', async() => {
-    const pokemon = await buscarPokemon('charizard');
+document.addEventListener('DOMContentLoaded', async () => {
+  const pokemon = await buscarPokemon('charizard');
+  console.log(pokemon);
 });
 
