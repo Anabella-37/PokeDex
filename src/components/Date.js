dedda278689fea618a4c3accf788fbdb40caab3c
Kevin-Illu <78170908+Kevin-Illu@users.noreply.github.com>
2022-11-20 02:05:28 -0600

    [ADD] a new config for better format code

--- src/components/Date.js
+++ src/components/Date.js
@@ -1,3 +1,5 @@
-export function showCurrentDate() {
-  const div = document.querySelector("#date");
+export default function showCurrentDate() {
+	const div = document.querySelector('#date');
+	const date = new Date();
+	div.appendChild(date);
 }
